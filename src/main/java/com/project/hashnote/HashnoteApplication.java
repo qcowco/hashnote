b4316c523054f3f2e74fab9cd252e34b458eb868
qcowco <kamil.szuberski.g@gmail.com>
2019-12-25 20:32:36 +0100

    Initialize project

---
+++ src/main/java/com/project/hashnote/HashnoteApplication.java
@@ -0,0 +1,13 @@
+package com.project.hashnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashnoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HashnoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/hashnote/HashnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hashnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HashnoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
