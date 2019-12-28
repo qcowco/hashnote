d28843deada0028f6ee34e1b1687696a45b59de9
qcowco <kamil.szuberski.g@gmail.com>
2019-12-28 02:36:18 +0100

    Update message encoder structure

--- src/main/java/com/project/hashnote/encoders/MessageEncoderImpl.java
+++ src/main/java/com/project/hashnote/encoders/MessageEncoderImpl.java
@@ -3,6 +3,7 @@ package com.project.hashnote.encoders;
 import com.project.hashnote.encoders.algorithms.AlgorithmDetails;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import org.apache.tomcat.util.codec.binary.Base64;
 
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
@@ -10,6 +11,7 @@ import javax.crypto.spec.SecretKeySpec;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.Random;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -18,38 +20,52 @@ public class MessageEncoderImpl implements MessageEncoder {
     private SecretKey secretKey;
     private IvParameterSpec initVector;
 
-    public byte[] encode(byte[] message) {
+    public  byte[] encode(byte[] message) {
         return tryEncode(message);
     }
 
-    private byte[] tryEncode(byte[] message) {
-        byte[] encodedMesssage;
+    private byte[] tryEncode(byte[] plainMessage) {
         try {
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey, initVector);
-            encodedMesssage = cipher.doFinal(message);
+            return getEncodedFrom(plainMessage);
         } catch (InvalidKeyException | BadPaddingException | IllegalBlockSizeException | InvalidAlgorithmParameterException e) {
             throw new RuntimeException(e);
         }
-        return encodedMesssage;
+    }
+
+    private byte[] getEncodedFrom(byte[] plainMessage) throws InvalidKeyException,
+            InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, initVector);
+
+        byte[] plainBytes = plainMessage;
+        byte[] encodedBytes = cipher.doFinal(plainBytes);
+
+        return encodedBytes;
     }
 
     public byte[] decode(byte[] encodedMessage) throws BadPaddingException {
-        return tryDecodeMessage(encodedMessage);
+        return tryDecode(encodedMessage);
     }
 
-    private byte[] tryDecodeMessage(byte[] encodedMessage) throws BadPaddingException {
-        byte[] decodedMessage;
+    private byte[] tryDecode(byte[] encodedMessage) throws BadPaddingException {
         try {
-            cipher.init(Cipher.DECRYPT_MODE, secretKey, initVector);
-            decodedMessage = cipher.doFinal(encodedMessage);
+            return getDecodedFrom(encodedMessage);
         } catch (InvalidKeyException | IllegalBlockSizeException | InvalidAlgorithmParameterException e) {
             throw new RuntimeException(e);
         }
-        return decodedMessage;
+    }
+
+    private byte[] getDecodedFrom(byte[] encodedMessage) throws InvalidKeyException,
+            InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, initVector);
+
+        byte[] encodedBytes = encodedMessage;
+        byte[] plainBytes = cipher.doFinal(encodedBytes);
+
+        return plainBytes;
     }
 
     public byte[] getPrivateKey(){
-        return secretKey.getEncoded();
+        return Base64.encodeBase64(secretKey.getEncoded());
     }
 
     public byte[] getInitVector(){
@@ -61,7 +77,7 @@ public class MessageEncoderImpl implements MessageEncoder {
     }
 
     public static class EncoderBuilder {
-        private AlgorithmDetails details;
+        private AlgorithmDetails algorithm;
         private Cipher cipher;
         private SecretKey secretKey;
         private IvParameterSpec initVector;
@@ -69,38 +85,50 @@ public class MessageEncoderImpl implements MessageEncoder {
         public EncoderBuilder(){}
 
         public EncoderBuilder algorithmDetails(AlgorithmDetails details){
-            this.details = details;
+            this.algorithm = details;
             return this;
         }
 
         public EncoderBuilder secretKey() throws NoSuchAlgorithmException {
-            KeyGenerator keyGenerator = KeyGenerator.getInstance(details.getMethod());
-            keyGenerator.init(details.getKeySize());
+            KeyGenerator keyGenerator = KeyGenerator.getInstance(algorithm.getMethod());
+            SecureRandom secureRandom = new SecureRandom();
+            keyGenerator.init(algorithm.getKeySize(), secureRandom);
+
             secretKey = keyGenerator.generateKey();
+
             return this;
         }
 
         public EncoderBuilder secretKey(byte[] customKey){
-            secretKey = new SecretKeySpec(customKey, 0, customKey.length, details.getMethod());
+            byte[] key = Base64.decodeBase64(customKey);
+
+            secretKey = new SecretKeySpec(key, 0, key.length, algorithm.getMethod());
+
             return this;
         }
 
         public EncoderBuilder initVector(){
-            byte[] randomVector = new byte[details.getVectorSize()];
-            new Random().nextBytes(randomVector);
-            initVector = new IvParameterSpec(randomVector);
+            byte[] randomBytes = new byte[algorithm.getVectorSize()];
+            new Random().nextBytes(randomBytes);
+
+            initVector = new IvParameterSpec(randomBytes);
+
             return this;
         }
 
         public EncoderBuilder initVector(byte[] customVector){
-            if(customVector.length != details.getVectorSize())
-                throw new IllegalArgumentException("Vector needs to be " + details.getVectorSize() + " bytes long, is: " + customVector.length);
+            if(customVector.length != algorithm.getVectorSize())
+                throw new IllegalArgumentException("Vector needs to be "
+                        + algorithm.getVectorSize() + " bytes long, is: " + customVector.length);
+
             initVector = new IvParameterSpec(customVector);
+
             return this;
         }
 
         private void cipher() throws NoSuchPaddingException, NoSuchAlgorithmException {
-            cipher = Cipher.getInstance(details.getMethod() + "/" + details.getMode() + "/" + details.getPadding());
+            cipher = Cipher.getInstance(algorithm.getMethod()
+                    + "/" + algorithm.getMode() + "/" + algorithm.getPadding());
         }
 
         public MessageEncoderImpl build() throws NoSuchAlgorithmException, NoSuchPaddingException {
