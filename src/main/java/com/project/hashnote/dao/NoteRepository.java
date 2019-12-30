9be13d17ce0a53079b0d1c647debf860d6471a89
qcowco <kamil.szuberski.g@gmail.com>
2019-12-30 01:02:53 +0100

    Add PATCH and DELETE endpoints

--- src/main/java/com/project/hashnote/dao/NoteRepository.java
+++ src/main/java/com/project/hashnote/dao/NoteRepository.java
@@ -4,4 +4,5 @@ import com.project.hashnote.document.Note;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface NoteRepository extends MongoRepository<Note, String> {
+    void deleteById(String id);
 }
--- src/main/java/com/project/hashnote/web/NoteController.java
+++ src/main/java/com/project/hashnote/web/NoteController.java
@@ -1,12 +1,14 @@
 package com.project.hashnote.web;
 
 import com.project.hashnote.dto.NoteDto;
+import com.project.hashnote.dto.NoteRequest;
 import com.project.hashnote.service.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.UnsupportedEncodingException;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
@@ -14,6 +16,7 @@ import java.util.List;
 public class NoteController {
     NoteService noteService;
 
+    // TODO: 29.12.2019 kody 4xx dla InorrectPrivateKey, MalformedPrivateKey, InvalidAlgorithmName
     @Autowired
     public NoteController(NoteService noteService) {
         this.noteService = noteService;
@@ -30,25 +33,24 @@ public class NoteController {
     }
 
     @GetMapping("/{id}/{secretKey}")
-    public NoteDto getOneDecoded(@PathVariable(name = "id") String id, @PathVariable(name = "secretKey") String secretKey) {
-        return noteService.getDecoded(id, secretKey);
+    public NoteDto getOneDecoded(@PathVariable(name = "id") String id, @PathVariable(name = "secretKey") String secretKey) { // TODO: 30.12.2019 validation keys
+        return noteService.getDecrypted(id, secretKey);
     }
 
-    @PostMapping // TODO: 28.12.2019 validation
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE) // TODO: 28.12.2019 validation
     @ResponseStatus(HttpStatus.CREATED)
-    public String save(@RequestBody NoteDto noteDto) {
-        return noteService.save(noteDto);
+    public String save(@RequestBody NoteRequest noteRequest) {
+        return noteService.save(noteRequest);
     }
 
-    @PutMapping
-    public String put(@RequestBody NoteDto noteDto){
-        // TODO: 28.12.2019 put
-        return null;
+    @PatchMapping("/{id}/{secretKey}")
+    public void patch(@RequestBody NoteRequest noteRequest, @PathVariable String id, @PathVariable String secretKey){ // TODO: 30.12.2019 validation
+        noteRequest.getNoteDto().setId(id);
+        noteService.patch(noteRequest, secretKey);
     }
 
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable String id){
-        // TODO: 28.12.2019 delete
-        return null;
+    @DeleteMapping("/{id}") // TODO: 30.12.2019 response code
+    public void delete(@PathVariable String id){
+        noteService.delete(id);
     }
 }
