447e23280e6862fcd03304599a8e9aeda34f71b0
qcowco <kamil.szuberski.g@gmail.com>
2020-03-11 20:12:22 +0100

    Create a tool for writing/reading jwts

---
+++ src/main/java/com/project/hashnote/security/util/JwtTokenUtil.java
@@ -0,0 +1,52 @@
+package com.project.hashnote.security.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Date;
+import java.util.function.Function;
+
+public class JwtTokenUtil {
+    @Value("${jwt.expire}")
+    private int TIME_TO_EXPIRE;
+    @Value("${jwt.secret}")
+    private String base64Secret;
+    private String token;
+
+    public String gerUsernameFromToken(String token) {
+        this.token = token;
+        return getClaimFromToken(Claims::getSubject);
+    }
+
+    public Date getExpirationDateFromToken(String token) {
+        this.token = token;
+        return getClaimFromToken(Claims::getExpiration);
+    }
+
+    public boolean isExpired(String token) {
+        Date now = new Date();
+        return getExpirationDateFromToken(token).before(now);
+    }
+
+    private <T> T getClaimFromToken(Function<Claims, T> claimsResolver) {
+        return claimsResolver.apply(getClaimsFromToken());
+    }
+
+    private Claims getClaimsFromToken() {
+        return Jwts.parser()
+                .setSigningKey(base64Secret)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + TIME_TO_EXPIRE))
+                .signWith(SignatureAlgorithm.HS512, base64Secret)
+                .compact();
+    }
+}
