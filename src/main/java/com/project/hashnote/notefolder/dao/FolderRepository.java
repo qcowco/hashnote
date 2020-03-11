0f158ddbfafb8cbddef2407656f309e4d906809b
qcowco <kamil.szuberski.g@gmail.com>
2020-03-11 20:10:46 +0100

    Move user and notefolder-related classes to their respective packages

--- src/main/java/com/project/hashnote/notefolder/dao/FolderRepository.java
+++ src/main/java/com/project/hashnote/notefolder/dao/FolderRepository.java
@@ -1,6 +1,6 @@
 package com.project.hashnote.dao;
 
-import com.project.hashnote.document.Folder;
+import com.project.hashnote.notefolder.document.Folder;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface FolderRepository extends MongoRepository<Folder, String> {
--- src/main/java/com/project/hashnote/user/dao/UserRepository.java
+++ src/main/java/com/project/hashnote/user/dao/UserRepository.java
@@ -1,6 +1,6 @@
 package com.project.hashnote.dao;
 
-import com.project.hashnote.document.User;
+import com.project.hashnote.user.document.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String> {
