6914afb9b0081fe79616bb3193dfb068061cbf79
qcowco <kamil.szuberski.g@gmail.com>
2020-03-15 23:00:30 +0100

    Create folder dtos

---
+++ src/main/java/com/project/hashnote/notefolder/dto/FolderRequest.java
@@ -0,0 +1,7 @@
+package com.project.hashnote.notefolder.dto;
+
+import lombok.Data;
+
+public @Data class FolderRequest {
+    private String name;
+}
---
+++ src/main/java/com/project/hashnote/notefolder/dto/FolderResponse.java
@@ -0,0 +1,9 @@
+package com.project.hashnote.notefolder.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+public @Data class FolderResponse {
+    private String folderId;
+}
