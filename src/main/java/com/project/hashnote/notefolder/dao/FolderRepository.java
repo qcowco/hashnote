13c6c71aa8395036a05a0bdfee971082503d7ea1
qcowco <kamil.szuberski.g@gmail.com>
2020-03-15 22:59:56 +0100

    Add find by author method

--- src/main/java/com/project/hashnote/notefolder/dao/FolderRepository.java
+++ src/main/java/com/project/hashnote/notefolder/dao/FolderRepository.java
@@ -1,7 +1,10 @@
-package com.project.hashnote.dao;
+package com.project.hashnote.notefolder.dao;
 
 import com.project.hashnote.notefolder.document.Folder;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface FolderRepository extends MongoRepository<Folder, String> {
+    List<Folder> findByAuthor(String username);
 }
