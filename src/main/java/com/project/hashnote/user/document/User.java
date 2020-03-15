f5a1fe92af580dd70e1cd43188ac9992ba81be6c
qcowco <kamil.szuberski.g@gmail.com>
2020-03-15 17:08:23 +0100

    Remove unnecessary note/note-folder references

--- src/main/java/com/project/hashnote/user/document/User.java
+++ src/main/java/com/project/hashnote/user/document/User.java
@@ -1,20 +1,13 @@
-package com.project.hashnote.document;
+package com.project.hashnote.user.document;
 
 import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@Builder
-@AllArgsConstructor
 @Document(collection = "users")
-public class User {
+public @Data class User {
     private String id;
     private String username;
     private String password;
-    private List<String> notes;
-    private List<String> folders;
 }
