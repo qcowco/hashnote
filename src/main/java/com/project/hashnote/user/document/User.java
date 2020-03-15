a8dda4da52ea886c1142cbf6aeb20570cdaa1a98
qcowco <kamil.szuberski.g@gmail.com>
2020-03-15 19:08:47 +0100

    Clean up lombok annotations

--- src/main/java/com/project/hashnote/user/document/User.java
+++ src/main/java/com/project/hashnote/user/document/User.java
@@ -3,11 +3,11 @@ package com.project.hashnote.user.document;
 import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.List;
-
 @Document(collection = "users")
 public @Data class User {
     private String id;
+    @NonNull
     private String username;
+    @NonNull
     private String password;
 }
