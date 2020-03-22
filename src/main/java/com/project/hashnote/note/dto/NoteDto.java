a4771397fb0c26d78bb20d5cbacbd93065649166
qcowco <kamil.szuberski.g@gmail.com>
2020-03-23 00:44:27 +0100

    Add date pattern

--- src/main/java/com/project/hashnote/note/dto/NoteDto.java
+++ src/main/java/com/project/hashnote/note/dto/NoteDto.java
@@ -1,10 +1,12 @@
 package com.project.hashnote.note.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
+import java.time.LocalDateTime;
 
 public @Data class NoteDto {
     @Null
@@ -15,4 +17,8 @@ public @Data class NoteDto {
     @NotNull
     @Length(min = 1, max = 20000)
     private String message;
+    @JsonFormat(pattern = "yyyy/MM/dd HH:mm:ss")
+    private LocalDateTime createdAt;
+    @JsonFormat(pattern = "yyyy/MM/dd HH:mm:ss")
+    private LocalDateTime expiresAt;
 }
