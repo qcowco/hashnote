c87d495d9312e9123339475d2dae9ca5256182ee
qcowco <kamil.szuberski.g@gmail.com>
2020-03-30 23:17:10 +0200

    Remove parent class methods

--- src/main/java/com/project/hashnote/encryption/algorithms/Aes256Details.java
+++ src/main/java/com/project/hashnote/encryption/algorithms/Aes256Details.java
@@ -1,19 +1,10 @@
 package com.project.hashnote.encryption.algorithms;
 
-import com.project.hashnote.encryption.exceptions.IncorrectAlgorithmPropertiesException;
+import lombok.Data;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-
 @Component
-public class Aes256Details implements AlgorithmDetails {
+public @Data class Aes256Details extends AlgorithmDetails {
     private final String method = "AES";
     private final String mode = "CBC";
     private final String padding = "PKCS5Padding";
@@ -21,74 +12,8 @@ public class Aes256Details implements AlgorithmDetails {
     private final int vectorSize = 128;
 
     @Override
-    public boolean isMethod(String method) {
-        return this.method.equals(method);
+    int getKeyLength() {
+        return keySize/8;
     }
-
-    @Override
-    public SecretKey randomSecretKey() {
-        KeyGenerator keyGenerator = tryGetKeyGenerator();
-        SecureRandom secureRandom = new SecureRandom();
-        keyGenerator.init(keySize, secureRandom);
-
-        return keyGenerator.generateKey();
-    }
-
-    private KeyGenerator tryGetKeyGenerator() {
-        try {
-            return KeyGenerator.getInstance(method);
-        } catch (NoSuchAlgorithmException e) {
-            throw new IncorrectAlgorithmPropertiesException("Incorrect algorithm.", e);
-        }
-    }
-
-    @Override
-    public SecretKey customSecretKey(byte[] customKey) {
-         return new SecretKeySpec(customKey, 0, customKey.length, method);
-    }
-
-    @Override
-    public boolean isKeyProperLength(byte[] key) {
-        return key.length == keySize/8;
-    }
-
-    public IvParameterSpec randomInitVector() {
-        byte[] randomBytes = new byte[getVectorByteSize()];
-        new SecureRandom().nextBytes(randomBytes);
-
-        return new IvParameterSpec(randomBytes);
-    }
-
-    private int getVectorByteSize() {
-        return vectorSize/8;
-    }
-
-    @Override
-    public IvParameterSpec customInitVector(byte[] customVector) {
-        return new IvParameterSpec(customVector);
-    }
-
-    @Override
-    public boolean isVectorProperLength(byte[] vector) {
-        return vector.length == getVectorByteSize();
-    }
-
-    @Override
-    public Cipher getCipher() {
-        return tryGetCipher();
-    }
-
-    private Cipher tryGetCipher()  {
-        try {
-            String transformation = method
-                    + "/" + mode + "/" + padding;
-
-            return Cipher.getInstance(transformation);
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
-            throw new IncorrectAlgorithmPropertiesException
-                    ("Provided algorithm's properties don't match up with expected values", e);
-        }
-    }
-
 }
 // TODO: 28.12.2019 add - triple DES, Blowfish, Twofish[, RSA?]
--- src/main/java/com/project/hashnote/encryption/algorithms/DESede168Details.java
+++ src/main/java/com/project/hashnote/encryption/algorithms/DESede168Details.java
@@ -1,19 +1,10 @@
 package com.project.hashnote.encryption.algorithms;
 
-import com.project.hashnote.encryption.exceptions.IncorrectAlgorithmPropertiesException;
+import lombok.Data;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-
 @Component
-public class DESede168Details implements AlgorithmDetails {
+public @Data class DESede168Details extends AlgorithmDetails {
     private final String method = "DESede";
     private final String mode = "CBC";
     private final String padding = "PKCS5Padding";
@@ -21,72 +12,7 @@ public class DESede168Details implements AlgorithmDetails {
     private final int vectorSize = 64;
 
     @Override
-    public boolean isMethod(String method) {
-        return this.method.equals(method);
-    }
-
-    @Override
-    public SecretKey randomSecretKey() {
-        KeyGenerator keyGenerator = tryGetKeyGenerator();
-        SecureRandom secureRandom = new SecureRandom();
-        keyGenerator.init(keySize, secureRandom);
-
-        return keyGenerator.generateKey();
-    }
-
-    private KeyGenerator tryGetKeyGenerator() {
-        try {
-            return KeyGenerator.getInstance(method);
-        } catch (NoSuchAlgorithmException e) {
-            throw new IncorrectAlgorithmPropertiesException("Incorrect algorithm.", e);
-        }
-    }
-
-    @Override
-    public SecretKey customSecretKey(byte[] customKey) {
-        return new SecretKeySpec(customKey, 0, customKey.length, method);
-    }
-
-    @Override
-    public boolean isKeyProperLength(byte[] key) {
-        return key.length == (keySize + 24)/8;
-    }
-
-    public IvParameterSpec randomInitVector() {
-        byte[] randomBytes = new byte[getVectorByteSize()];
-        new SecureRandom().nextBytes(randomBytes);
-
-        return new IvParameterSpec(randomBytes);
-    }
-
-    private int getVectorByteSize() {
-        return vectorSize/8;
-    }
-
-    @Override
-    public IvParameterSpec customInitVector(byte[] customVector) {
-        return new IvParameterSpec(customVector);
-    }
-
-    @Override
-    public boolean isVectorProperLength(byte[] vector) {
-        return vector.length == getVectorByteSize();
-    }
-
-    @Override
-    public Cipher getCipher() {
-        return tryGetCipher();
-    }
-
-    private Cipher tryGetCipher()  {
-        try {
-            String transformation = method
-                    + "/" + mode + "/" + padding;
-
-            return Cipher.getInstance(transformation);
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
-            throw new IncorrectAlgorithmPropertiesException
-                    ("Provided algorithm's properties don't match up with expected values", e);
-        }
+    int getKeyLength() {
+        return (keySize + 24)/8;
     }
 }
