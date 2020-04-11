3cda41590164f4d9e0edf566cc1e2e0c3b94a5d7
qcowco <kamil.szuberski.g@gmail.com>
2020-04-11 04:41:07 +0200

    Configure for standalone tomcat deployment

--- src/main/java/com/project/hashnote/HashnoteApplication.java
+++ src/main/java/com/project/hashnote/HashnoteApplication.java
@@ -2,9 +2,16 @@ package com.project.hashnote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HashnoteApplication {
+public class HashnoteApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HashnoteApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HashnoteApplication.class, args);
