5b2a91e694733b0da4379489dc01c4af8709bd2f
qcowco <kamil.szuberski.g@gmail.com>
2020-04-16 01:32:42 +0200

    Add bearer token support

--- src/main/java/com/project/hashnote/swagger/configuration/SpringFoxConfig.java
+++ src/main/java/com/project/hashnote/swagger/configuration/SpringFoxConfig.java
@@ -1,5 +1,6 @@
 package com.project.hashnote.swagger.configuration;
 
+import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.hateoas.client.LinkDiscoverer;
@@ -7,11 +8,20 @@ import org.springframework.hateoas.client.LinkDiscoverers;
 import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
 import org.springframework.plugin.core.SimplePluginRegistry;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -22,7 +32,9 @@ public class SpringFoxConfig {
                 .select()
                 .paths(path -> path.startsWith("/api/v1") || path.startsWith("/login") || path.startsWith("/register"))
                 .build()
-                .ignoredParameterTypes(AuthenticationPrincipal.class);
+                .ignoredParameterTypes(AuthenticationPrincipal.class)
+                .securitySchemes(Lists.newArrayList(apiKey()))
+                .securityContexts(Arrays.asList(securityContext()));
     }
 
     @Bean
@@ -31,6 +43,32 @@ public class SpringFoxConfig {
         plugins.add(new CollectionJsonLinkDiscoverer());
         return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
     }
+
+    @Bean
+    public SecurityConfiguration securityConfiguration() {
+        return SecurityConfigurationBuilder.builder()
+                .scopeSeparator(",")
+                .useBasicAuthenticationWithAccessCodeGrant(false)
+                .build();
+
     }
 
+    private ApiKey apiKey() {
+        return new ApiKey("Bearer", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+
+        AuthorizationScope[] authorizationScopes = { authorizationScope };
+
+        return Arrays.asList(new SecurityReference("Bearer", authorizationScopes));
+    }
 }
