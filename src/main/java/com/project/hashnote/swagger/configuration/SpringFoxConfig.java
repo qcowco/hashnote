a583e93439740fc5e3442c20eb3493df88b3cbfb
qcowco <kamil.szuberski.g@gmail.com>
2020-04-16 01:31:29 +0200

    Add LinkDiscoverers Bean

--- src/main/java/com/project/hashnote/swagger/configuration/SpringFoxConfig.java
+++ src/main/java/com/project/hashnote/swagger/configuration/SpringFoxConfig.java
@@ -2,10 +2,16 @@ package com.project.hashnote.swagger.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @Configuration
 @EnableSwagger2
@@ -17,6 +23,14 @@ public class SpringFoxConfig {
                 .paths(path -> path.startsWith("/api/v1") || path.startsWith("/login") || path.startsWith("/register"))
                 .build()
                 .ignoredParameterTypes(AuthenticationPrincipal.class);
+    }
+
+    @Bean
+    public LinkDiscoverers discoverers() {
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+    }
     }
 
 }
