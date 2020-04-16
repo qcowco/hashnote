748cd071cf5ddaf6f6b9430396ae6f69098e0c54
qcowco <kamil.szuberski.g@gmail.com>
2020-04-16 02:15:11 +0200

    Add secret key emptiness check method

--- src/main/java/com/project/hashnote/note/dto/EncryptionResponse.java
+++ src/main/java/com/project/hashnote/note/dto/EncryptionResponse.java
@@ -3,9 +3,14 @@ package com.project.hashnote.note.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.hateoas.RepresentationModel;
+import org.springframework.util.StringUtils;
 
 @AllArgsConstructor
 public @Data class EncryptionResponse extends RepresentationModel<EncryptionResponse> {
     private String noteId;
     private String secretKey;
+
+    public boolean hasSecretKey() {
+        return StringUtils.hasText(secretKey);
+    }
 }
