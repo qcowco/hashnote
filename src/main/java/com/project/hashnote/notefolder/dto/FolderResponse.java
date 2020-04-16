746423a896a96e1b8f9716d165f8b6c4b32c3e0d
qcowco <kamil.szuberski.g@gmail.com>
2020-04-16 02:51:12 +0200

    Add HATEOAS support

--- src/main/java/com/project/hashnote/notefolder/dto/FolderResponse.java
+++ src/main/java/com/project/hashnote/notefolder/dto/FolderResponse.java
@@ -2,8 +2,9 @@ package com.project.hashnote.notefolder.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.hateoas.RepresentationModel;
 
 @AllArgsConstructor
-public @Data class FolderResponse {
+public @Data class FolderResponse extends RepresentationModel<FolderResponse> {
     private String folderId;
 }
